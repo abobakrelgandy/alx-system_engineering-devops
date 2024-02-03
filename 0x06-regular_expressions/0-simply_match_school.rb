3612217fba3dfc6d965735f4a59f56eba65e8322
abobakrelgandy <abobakrelgandy338@gmail.com>
2024-02-02 18:10:42 -0800

    my task

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,3 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# The regular expression must match School
-
-puts ARGV[0].scan(/School/).join
+reg_a = /School/
+puts ARGV[0].scan(reg_a).join
